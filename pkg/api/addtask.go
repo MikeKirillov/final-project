eac8cebc128a68a8803ce4bc31d921f653c18597
Mike Kirillov <kirilov.mikhail91@yandex.ru>
2026-02-01 22:35:58 +0300

    made response creating easier

--- pkg/api/addtask.go
+++ pkg/api/addtask.go
@@ -12,49 +12,29 @@ import (
 	"time"
 )
 
-type AddTaskRs struct {
-	Id string `json:"id"`
-}
-
-type ErrorRs struct {
-	Err string `json:"error"`
-}
-
 func addTaskHandler(w http.ResponseWriter, req *http.Request) {
 	var task db.Task
-	var addTaskRs AddTaskRs
-	var errorRs ErrorRs
-
 	readJson(w, req, &task)
 
 	if len(task.Title) == 0 {
 		err := errors.New("title must not be empty")
-		errorRs.Err = err.Error()
-		writeJson(w, http.StatusBadRequest, &errorRs)
+		writeJson(w, http.StatusBadRequest, err.Error())
 		return
 	}
-
 	if err := checkRepeat(&task); err != nil {
-		errorRs.Err = err.Error()
-		writeJson(w, http.StatusBadRequest, &errorRs)
+		writeJson(w, http.StatusBadRequest, err.Error())
 		return
 	}
-
 	if err := checkDate(&task); err != nil {
-		errorRs.Err = err.Error()
-		writeJson(w, http.StatusBadRequest, &errorRs)
+		writeJson(w, http.StatusBadRequest, err.Error())
 		return
 	}
-
 	id, err := db.AddTask(&task)
 	if err != nil {
-		errorRs.Err = err.Error()
-		writeJson(w, http.StatusBadRequest, &errorRs)
+		writeJson(w, http.StatusBadRequest, err.Error())
 		return
 	}
-
-	addTaskRs.Id = strconv.FormatInt(id, 10)
-	writeJson(w, http.StatusOK, &addTaskRs)
+	writeJson(w, http.StatusOK, strconv.FormatInt(id, 10))
 }
 
 func readJson(w http.ResponseWriter, req *http.Request, task *db.Task) {
@@ -65,18 +45,24 @@ func readJson(w http.ResponseWriter, req *http.Request, task *db.Task) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
 	if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 }
 
-func writeJson(w http.ResponseWriter, status int, data any) {
+func writeJson(w http.ResponseWriter, status int, data string) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(status)
 
-	if err := json.NewEncoder(w).Encode(data); err != nil {
+	responsMap := make(map[string]string)
+
+	if status == http.StatusOK {
+		responsMap["id"] = data
+	} else {
+		responsMap["error"] = data
+	}
+	if err := json.NewEncoder(w).Encode(responsMap); err != nil {
 		http.Error(w, "error encoding JSON", http.StatusInternalServerError)
 	}
 }
@@ -84,7 +70,7 @@ func writeJson(w http.ResponseWriter, status int, data any) {
 func checkDate(task *db.Task) error {
 	now := time.Now()
 	nowString := now.Format(LAYOUT)
-	//if task.Date is an empty string then put time.Now() into
+	// if task.Date is an empty string then put time.Now() into
 	if len(task.Date) == 0 {
 		task.Date = nowString
 	}
