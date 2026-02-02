14ff5e6f9f3f58872b890c65b2665925858057f1
Mike Kirillov <kirilov.mikhail91@yandex.ru>
2026-02-02 17:12:56 +0300

    added selecting by limit logic

--- pkg/db/task.go
+++ pkg/db/task.go
@@ -17,6 +17,9 @@ const INSERT = `
 INSERT INTO scheduler (date, title, comment, repeat)
 	VALUES (:date, :title, :comment, :repeat)`
 
+const SELECT_BY_LIMIT = `
+SELECT * FROM scheduler ORDER BY date LIMIT ?`
+
 func AddTask(task *Task) (int64, error) {
 	var id int64
 
@@ -37,11 +40,26 @@ func AddTask(task *Task) (int64, error) {
 }
 
 func Tasks(limit int) ([]*Task, error) {
-	// rows, err := db.Query("")
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return nil, err
-	// }
+	var tasks []*Task
 
-	return nil, nil
+	rows, err := db.Query(SELECT_BY_LIMIT, limit)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		task := Task{}
+
+		err := rows.Scan(&task.ID, &task.Date, &task.Title, &task.Comment, &task.Repeat)
+		if err != nil {
+			log.Println(err)
+			return tasks, err
+		}
+
+		tasks = append(tasks, &task)
+	}
+
+	return tasks, nil
 }
