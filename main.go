a89c53533048575ea1578d09dd68f43ffbc0c904
Mike Kirillov <kirilov.mikhail91@yandex.ru>
2026-02-05 20:43:31 +0300

    adding get environment feature

--- main.go
+++ main.go
@@ -4,9 +4,17 @@ import (
 	"final-project/pkg/db"
 	"final-project/pkg/server"
 	"log"
+	"os"
 )
 
+const TODO_PORT = "TODO_PORT"
+
 func main() {
+	// allow to run like: TODO_PORT=<port_number> go run .
+	port := os.Getenv(TODO_PORT)
+
+	log.Println("port is: " + port)
+
 	err := db.Init("scheduler.db")
 
 	if err != nil {
